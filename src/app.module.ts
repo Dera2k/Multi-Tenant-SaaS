1b2cbee25d3c6336d983f9e7181524b1a59c7e0a
Nwogu Chidera <chideranwogu2003@gmail.com>
2026-02-09 22:09:52 +0100

    feat: registered new modules in the AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { UnitsModule } from './units/units.module';
+import { MembershipsModule } from './memberships/memberships.module';
+import { RolesModule } from './roles/roles.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, UnitsModule, MembershipsModule, RolesModule, WorkspacesModule, ProjectsModule],
   controllers: [AppController],
   providers: [AppService],
 })
