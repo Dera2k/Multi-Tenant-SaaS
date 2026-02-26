88af25debf24229708e15a3309b9531d777f2a41
Nwogu Chidera <chideranwogu2003@gmail.com>
2026-02-26 20:36:00 +0100

    added a create user, unit response and update user dtos

---
+++ src/units/dtos/create-unit.dto.ts
@@ -0,0 +1,14 @@
+import { IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
+
+export class CreateUnitDto {
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(2)
+    @MaxLength(100)
+    name!: string;
+
+    @IsOptional()
+    @IsString()
+    @MaxLength(500)
+    description!: string;
+}
